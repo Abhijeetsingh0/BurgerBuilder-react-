e0cddb365382c6deace0f02db1f6f1f97a7923d2
Abhijeetsingh0 <67339987+Abhijeetsingh0@users.noreply.github.com>
2021-06-18 10:51:07 +0530

    Logout working

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -12,9 +12,9 @@ const authStart = ( state, action ) => {
     return updateObject( state, { error: null, loading: true } );
 };
 
-const authLogout = (state, action) =>{
-    return updateObject(state , {token:null , userId: null });
-}
+// const authLogout = (state, action) =>{
+//     return updateObject(state , {token:null , userId: null });
+// }
 
 const authSuccess = (state, action) => {
     return updateObject( state, { 
@@ -32,15 +32,16 @@ const authFail = (state, action) => {
     });
 };
 
-// const authLogout = (state, action) => {
-//     return updateObject(state, { token: null, userId: null });
-// };
+const authLogout = (state, action) => {
+    return updateObject(state, { token: null, userId: null });
+};
 
 const reducer = ( state = initialState, action ) => {
     switch ( action.type ) {
         case actionTypes.AUTH_START: return authStart(state, action);
         case actionTypes.AUTH_SUCCESS: return authSuccess(state, action);
         case actionTypes.AUTH_FAIL: return authFail(state, action);
+        case actionTypes.AUTH_LOGOUT: return authLogout(state , action)
         default:
             return state;
     }
